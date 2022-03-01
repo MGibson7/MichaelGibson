2d2c432917776ed7c79b8be165b19a51bf3e36e3
MGibson7 <MGibsonASC@gmail.com>
2022-03-01 13:26:30 -0500

    new button styling

--- newportfolio.js
+++ newportfolio.js
@@ -3,13 +3,16 @@ work = document.getElementById("work");
 
 work.addEventListener("click", function(){
   document.getElementById("buttonClick").innerHTML = "<img src = './img/dtv.png' alt = 'DirecTV' width = 100px, height = 100px>";
-  document.getElementById("buttonClick2").innerHTML = "<h3>Business Analyst for American Communications 2013-Present</h3>";
-  document.getElementById("buttonClick3").innerHTML = "<style>ul{color:black;} li{margin-bottom: 10px;}</style> <ul><li> Produced daily, monthly, quarterly, and annual reports utilizing MySQL to showcase the revenue generated from each client,\
-   identify trends and similarities among the newly acquired customers, and monitor the health of the existing one thousand plus accounts that make up ACs “DirecTV for Business” customer base. This has contributed to the companys\
-    new customer acquisition cost being just 88% of what it was in 2012 and the existing customer churn has improved exponentially as well when adjusted for industry factors.\
-     Throughout this time DirecTVs number of paying subscribers decreased by an estimated 35% while ACs number increased by 337%.    </li>\
-     <li> Automated elements of monthly reporting and daily contractor invoicing by constructing a Python program which featured web scraping via selenium, functions, and loops.\
-      With this automation came the elimination of simple human errors within the invoicing which has saved an undefined but significant amount of capital and time. \
-      Contractor invoice audits changed from a weekly occurrence to now mostly non-existent. </li></ul>";
+  document.getElementById("buttonClick2").innerHTML = "<h3>Business Analyst for American Communications</h3>";
+  document.getElementById("buttonClick3").innerHTML = "<p>2013-Present</p>";
+
+});
+
+school = document.getElementById("school");
+
+school.addEventListener("click", function(){
+  document.getElementById("buttonClick").innerHTML = "<img src = './img/uncw.png' alt = 'UNCW' width = 100px, height = 100px> ";
+  document.getElementById("buttonClick2").innerHTML = "<h3>Bachelor of Science in Business Administration</h3>";
+  document.getElementById("buttonClick3").innerHTML = "<p>Graduated December 2013</p>";
 
 });
